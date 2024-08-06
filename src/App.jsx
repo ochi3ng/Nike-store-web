58e7886576f0d614392330b29fb10452b92e185a
ochi3ng <johnochi3ng@gmail.com>
2024-08-06 18:42:19 +0300

    working on footer and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Header, Sales,  } from './components'
-import {heroapi, popularsales,topratesales} from './data/data.js'
+import { FlexContent, Footer, Hero, Navbar, Sales, Stories,  } from './components'
+import {footerAPI, heroapi, highlight, popularsales,sneaker,story,topratesales} from './data/data.js'
 
 const App = () => {
   return (
     <>
-    <Header heroapi={heroapi}/>
-      <Sales endpoint={popularsales} />
-      <Sales endpoint={topratesales} />
+    <Navbar/>
+    <main className='flex flex-col gap-16 relative'>
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} />
+        <Sales endpoint={topratesales} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story}/>
+      
+    </main>
+      <Footer footerAPI={footerAPI}/>
     </>
   )
 }
